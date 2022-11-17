589ee2181d1c451303b8976da926ffd000a15a48
rishabh-live <rishabh12536@gmail.com>
2022-11-17 17:05:13 +0530

    session management completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
 import { Router, NavigationCancel, NavigationEnd } from '@angular/router';
-import { Location, LocationStrategy, PathLocationStrategy } from '@angular/common';
+import {
+    Location,
+    LocationStrategy,
+    PathLocationStrategy,
+} from '@angular/common';
 import { filter } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 declare let $: any;
 
 @Component({
@@ -9,33 +14,75 @@ declare let $: any;
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
     providers: [
-        Location, {
+        Location,
+        {
             provide: LocationStrategy,
-            useClass: PathLocationStrategy
-        }
-    ]
+            useClass: PathLocationStrategy,
+        },
+    ],
 })
 export class AppComponent {
-
     location: any;
     routerSubscription: any;
 
-    constructor(private router: Router) {}
+    constructor(private router: Router, private http: HttpClient) {}
 
-    ngOnInit(){
+    ngOnInit() {
         this.recallJsFuntions();
+        this.checkIsLoggedIn();
     }
 
     recallJsFuntions() {
         this.routerSubscription = this.router.events
-        .pipe(filter(event => event instanceof NavigationEnd || event instanceof NavigationCancel))
-        .subscribe(event => {
-            this.location = this.router.url;
-            if (!(event instanceof NavigationEnd)) {
-                return;
-            }
-            window.scrollTo(0, 0);
-        });
+            .pipe(
+                filter(
+                    (event) =>
+                        event instanceof NavigationEnd ||
+                        event instanceof NavigationCancel
+                )
+            )
+            .subscribe((event) => {
+                this.location = this.router.url;
+                if (!(event instanceof NavigationEnd)) {
+                    return;
+                }
+                window.scrollTo(0, 0);
+            });
     }
-    
+
+    private async checkIsLoggedIn() {
+        let session_id = sessionStorage.getItem('session_id');
+
+        if (!session_id) {
+            console.log('No Session ID');
+            this.http
+                .get('http://api.mitrafintech.com/wfh/session?new=true')
+                .subscribe((response) => {
+                    interface ReposnseObject {
+                        status: string;
+                        session_id: string;
+                    }
+                    let json: ReposnseObject = JSON.parse(
+                        JSON.stringify(response)
+                    );
+                    console.log(json);
+                    sessionStorage.setItem('session_id', json.session_id);
+                });
+        } else {
+
+            this.http
+                .get('http://api.mitrafintech.com/wfh/session?new=false&session_id='+session_id)
+                .subscribe((response) => {
+                    interface ReposnseObject {
+                        status: string;
+                        isUserLoggedIn: boolean;
+                    }
+                    let json: ReposnseObject = JSON.parse(
+                        JSON.stringify(response)
+                    );
+                    console.log(json.isUserLoggedIn);
+                    sessionStorage.setItem('isUserLoggedIn', JSON.stringify(json.isUserLoggedIn));
+                });
+        }
+    }
 }
