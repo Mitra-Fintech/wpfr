4d35f1c7c60c6be9e3fe8ee40677cda4b27c2a3d
RONIT SINGH <ronitsingh7003@gmail.com>
2022-11-18 23:09:35 +0530

    Hiding button after login

--- src/app/components/common/navbar/navbar.component.ts
+++ src/app/components/common/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-navbar',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+    isUserLoggedIn = false
+    i = 0;
 
-    constructor() { }
+    constructor() {
+        
+     }
 
-    ngOnInit(): void {}
+    ngOnInit(): void {
+        console.log("Url Change" + this.i+1);
+        this.i++;
+        // this.isUserLoggedIn = sessionStorage.getItem('isUserLoggedIn')
+        console.log(this.isUserLoggedIn);
+        let status = sessionStorage.getItem('isUserLoggedIn');
+        if(status == "true")
+            this.isUserLoggedIn = true;
+    }
 
     switcherClassApplied = false;
     switcherToggleClass() {
