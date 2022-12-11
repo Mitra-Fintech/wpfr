55673442cad82e403f74f97a499bfa9f1900eabd
rishabh-live <rishabh12536@gmail.com>
2022-12-11 17:55:40 +0530

    post a job api completed

--- src/app/components/pages/post-a-job-page/post-a-job-page.component.ts
+++ src/app/components/pages/post-a-job-page/post-a-job-page.component.ts
@@ -44,4 +44,65 @@ export class PostAJobPageComponent implements OnInit, OnDestroy {
         this.editor.destroy();
     }
 
+    createJob(){
+        let title = (<HTMLInputElement>document.getElementById('title')).value;
+        let description = (<HTMLInputElement>document.getElementById('description')).value;
+        let category_id = (<HTMLInputElement>document.getElementById('category_id')).value;
+        let type_id = (<HTMLInputElement>document.getElementById('type_id')).value;
+        let tags = (<HTMLInputElement>document.getElementById('tags')).value;
+        let gender = (<HTMLInputElement>document.getElementById('gender')).value;
+        let salary_type = (<HTMLInputElement>document.getElementById('salary_type')).value;
+        let minimum_salary = (<HTMLInputElement>document.getElementById('minimum_salary')).value;
+        let maximum_salary = (<HTMLInputElement>document.getElementById('maximum_salary')).value;
+        let experience = (<HTMLInputElement>document.getElementById('experience')).value;
+        let career_level = (<HTMLInputElement>document.getElementById('career_level')).value;
+        let qualification = (<HTMLInputElement>document.getElementById('qualification')).value;
+        let introduction_video_url = (<HTMLInputElement>document.getElementById('introduction_video_url')).value;
+        let deadline = (<HTMLInputElement>document.getElementById('deadline')).value;
+        let friendly_address = (<HTMLInputElement>document.getElementById('friendly_address')).value;
+        let location = (<HTMLInputElement>document.getElementById('location')).value;
+       
+        // let otp = (<HTMLInputElement>document.getElementById('otp')).value;
+
+        let session_id =
+        sessionStorage.getItem('session_id') || 'no-session';
+    session_id = session_id.replace('"', '');
+        
+        let body = new URLSearchParams();
+
+        body.set('title', title);
+        body.set('description', description);
+        body.set('category_id', category_id)
+        body.set('type_id',type_id)
+        body.set('tags', tags)
+        body.set('gender', gender)
+        body.set('salary_type', salary_type)
+        body.set('minimum_salary', minimum_salary)
+        body.set('maximum_salary', maximum_salary)
+        body.set('experience', experience)
+        body.set('career_level', career_level)
+        body.set('qualification', qualification)
+        body.set('introduction_video_url', introduction_video_url)
+        body.set('deadline', deadline)
+        body.set('friendly_address', friendly_address)
+        body.set('location', location)
+        body.set('session_id', session_id.replace('"', ''));
+    
+
+        this.http
+        .get('https://workfromhome.world/api/job/post?' + body)
+        .subscribe((response) => {
+            interface ReposnseObject {
+                status: string;
+                status_code: any;
+                isUserLoggedIn: boolean;
+                message: any;
+            }
+            let json: ReposnseObject = JSON.parse(JSON.stringify(response));
+            // console.log(json.isUserLoggedIn);
+            console.log(response);
+        })
+
+    }
+
 }
