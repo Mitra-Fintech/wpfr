a23a479c9332df2a7ab16bf197b5b424fa5c9b50
rishabh-live <rishabh12536@gmail.com>
2022-12-14 14:27:21 +0530

    dashboard opens only with right credits

--- src/app/components/pages/candidate-dashboard/candidate-dashboard-page.component.ts
+++ src/app/components/pages/candidate-dashboard/candidate-dashboard-page.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-candidate-dashboard-page',
@@ -7,9 +9,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CandidateDashboardPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit(): void {
+    this.checkIsLoggedIn()
   }
+
+  private async checkIsLoggedIn() {
+    let isUserLoggedIn = sessionStorage.getItem('session_id');
+
+    if (isUserLoggedIn == null) {
+        console.log('No Session ID');
+        this.http
+            .get('https://workfromhome.world/api/session/create')
+            .subscribe((response) => {
+                interface ReposnseObject {
+                    userType: string;
+                    isUserLoggedIn: boolean;
+                    userId : string;
+                    session_id: string;
+                }
+                let json: ReposnseObject = JSON.parse(
+                    JSON.stringify(response)
+                );
+                // console.log(json);
+                // sessionStorage.setItem('isUserLoggedIn', JSON.stringify(json.isUserLoggedIn));
+                // sessionStorage.setItem('userType', JSON.stringify(json.userType));
+                // sessionStorage.setItem('userId', JSON.stringify(json.userId));
+                // sessionStorage.setItem('session_id', JSON.stringify(json.session_id));
+
+                if((json.isUserLoggedIn == true ) && json.userType == "candidate")
+                {}else{
+                    this.router.navigateByUrl('/candidate');
+                }
+
+            });
+    } else {
+        let session_id = sessionStorage.getItem('session_id') || '';
+        session_id = session_id.replace('"','');
+
+
+        this.http
+            .get('https://workfromhome.world/api/session/get?session_id=' + session_id?.replace('"',''))
+            .subscribe((response) => {
+                interface ReposnseObject {
+                    userType: string;
+                    isUserLoggedIn: boolean;
+                    userId : string;
+                }
+                let json: ReposnseObject = JSON.parse(
+                    JSON.stringify(response)
+                );
+                if((json.isUserLoggedIn == true ) && json.userType == "candidate")
+                {}else{
+                    this.router.navigateByUrl('/candidate');
+                }
+            });
+    }
+}
 
 }
--- src/app/components/pages/employer-dashboard/employer-dashboard-page.component.ts
+++ src/app/components/pages/employer-dashboard/employer-dashboard-page.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-employer-dashboard-page',
@@ -7,12 +9,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployerDashboardPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit(): void {
+    this.checkIsLoggedIn();
   }
 
-  onLoad(){
-      
-  }
+  private async checkIsLoggedIn() {
+    let isUserLoggedIn = sessionStorage.getItem('session_id');
+
+    if (isUserLoggedIn == null) {
+        console.log('No Session ID');
+        this.http
+            .get('https://workfromhome.world/api/session/create')
+            .subscribe((response) => {
+                interface ReposnseObject {
+                    userType: string;
+                    isUserLoggedIn: boolean;
+                    userId : string;
+                    session_id: string;
+                }
+                let json: ReposnseObject = JSON.parse(
+                    JSON.stringify(response)
+                );
+                // console.log(json);
+                // sessionStorage.setItem('isUserLoggedIn', JSON.stringify(json.isUserLoggedIn));
+                // sessionStorage.setItem('userType', JSON.stringify(json.userType));
+                // sessionStorage.setItem('userId', JSON.stringify(json.userId));
+                // sessionStorage.setItem('session_id', JSON.stringify(json.session_id));
+
+                if((json.isUserLoggedIn == true ) && json.userType == "company")
+                {}else{
+                    this.router.navigateByUrl('/company');
+                }
+
+            });
+    } else {
+        let session_id = sessionStorage.getItem('session_id') || '';
+        session_id = session_id.replace('"','');
+
+
+        this.http
+            .get('https://workfromhome.world/api/session/get?session_id=' + session_id?.replace('"',''))
+            .subscribe((response) => {
+                interface ReposnseObject {
+                    userType: string;
+                    isUserLoggedIn: boolean;
+                    userId : string;
+                }
+                let json: ReposnseObject = JSON.parse(
+                    JSON.stringify(response)
+                );
+                // console.log(json);
+                if((json.isUserLoggedIn == true ) && json.userType == "company")
+                {}else{
+                    this.router.navigateByUrl('/company');
+                }
+            });
+    }
+}
 }
