180de3e28a93cbf8984354c464c45940699e077e
rishabh-live <rishabh12536@gmail.com>
2022-12-15 21:08:15 +0530

    mxt

--- src/app/components/pages/employer-dashboard/employer-dashboard-page.component.ts
+++ src/app/components/pages/employer-dashboard/employer-dashboard-page.component.ts
@@ -25,7 +25,7 @@ export class EmployerDashboardPageComponent implements OnInit {
     user_id = user_id.replace('"', '').replace('"', '');
 
     this.http
-                .get('https://workfromhome.world/api/job/list' + '?company_id=' + user_id)
+                .get('https://workfromhome.world/api/job/recent' + '?limit=2')
                 .subscribe((response) => {
 
                     interface ResponseObject {
