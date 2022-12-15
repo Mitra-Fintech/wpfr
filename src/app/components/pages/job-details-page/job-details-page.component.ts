c6ebd7d58e73a2f251b2cefa9ad78a778d579aa7
rishabh-live <rishabh12536@gmail.com>
2022-12-15 19:58:42 +0530

    Job Details Apply Only open when candidate account

--- src/app/components/pages/job-details-page/job-details-page.component.ts
+++ src/app/components/pages/job-details-page/job-details-page.component.ts
@@ -1,75 +1,73 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import * as module  from 'src/app/components/pages/job-listings-page/job-listings-page.component'
+import * as module from 'src/app/components/pages/job-listings-page/job-listings-page.component';
 import { environment } from 'src/environments/environment';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-job-details-page',
-  templateUrl: './job-details-page.component.html',
-  styleUrls: ['./job-details-page.component.scss']
+    selector: 'app-job-details-page',
+    templateUrl: './job-details-page.component.html',
+    styleUrls: ['./job-details-page.component.scss'],
 })
-
 export class JobDetailsPageComponent implements OnInit {
-  // objToArray: any;
-  public job_details: any;
-  public id: any;
-  public jobToArray: any;
-  
-  // public job_listing_data!: string;
+    // objToArray: any;
+    public job_details: any;
+    public id: any;
+    public jobToArray: any;
+    public postajob = false;
 
-  constructor(private http: HttpClient, private router: Router) { }
-  // constructor(private _Activatedroute:ActivatedRoute) { }
+    // public job_listing_data!: string;
 
+    constructor(private http: HttpClient, private router: Router) {}
+    // constructor(private _Activatedroute:ActivatedRoute) { }
 
-  ngOnInit(): void {
+    ngOnInit(): void {
+        this.getJobDetails();
+        let postajobstatus = sessionStorage.getItem('post-a-job');
+        if (postajobstatus == 'true') {
+            this.postajob = true;
+        }
+    }
 
-    this.getJobDetails();
+    getJobDetails() {
+        // console.log(environment.apiHost);
 
-  }
+        this.job_details = localStorage.getItem('job_id');
 
-  getJobDetails(){
+        this.job_details = JSON.parse(this.job_details);
 
-    // console.log(environment.apiHost);
+        // console.log(typeof this.job_details)
 
-    this.job_details = localStorage.getItem('job_id');
+        this.http
+            .get(
+                'https://workfromhome.world/api/job/details?job_id=' +
+                    this.job_details
+            )
+            .subscribe((response: any) => {
+                interface ResponseObject {
+                    status: string;
+                    code: any;
+                    data: Object;
+                    // session_id: string;
+                }
 
-    this.job_details = JSON.parse(this.job_details);
+                interface DataArrayObject {
+                    // job_title: string;
+                    array: Object;
+                }
 
-    // console.log(typeof this.job_details)
+                let responseObj: ResponseObject = JSON.parse(
+                    JSON.stringify(response)
+                );
 
-    this.http
-                .get('https://workfromhome.world/api/job/details?job_id=' + this.job_details)
-                .subscribe((response: any) => {
+                let dataJson: DataArrayObject = JSON.parse(
+                    JSON.stringify(responseObj.data)
+                );
 
-                    interface ResponseObject {
-                        status: string;
-                        code: any;
-                        data : Object;
-                        // session_id: string;
-                    }
+                this.jobToArray = Object.entries(dataJson);
 
-                    interface DataArrayObject {
-                        // job_title: string;
-                        array: Object;
-                    }
-
-                    let responseObj: ResponseObject = JSON.parse(
-                        JSON.stringify(response)
-                    );
-
-                    let dataJson: DataArrayObject = JSON.parse(
-                        JSON.stringify(responseObj.data)
-                    );
-                    
-                    this.jobToArray = Object.entries(dataJson);
-                    
-                    console.log(this.jobToArray[0][1]);
-                    
-                });
-
-
-    
-  }
+                console.log(this.jobToArray[0][1]);
+            });
+    }
 }
