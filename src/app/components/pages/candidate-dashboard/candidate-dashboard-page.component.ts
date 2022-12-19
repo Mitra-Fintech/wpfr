780d5a0c12a523a68a01b93fbd229d1fd32fb649
Vivaan Shiromani <shiromanivivan@gmail.com>
2022-12-19 18:44:14 +0530

    candidate-dashboard-function-uncomment

--- src/app/components/pages/candidate-dashboard/candidate-dashboard-page.component.ts
+++ src/app/components/pages/candidate-dashboard/candidate-dashboard-page.component.ts
@@ -20,7 +20,7 @@ export class CandidateDashboardPageComponent implements OnInit {
 
     ngOnInit(): void {
         this.checkIsLoggedIn()
-        // this.getJobListing();
+        this.getJobListing();
         this.getAppliedJobListing();
     }
     private async checkIsLoggedIn() {
