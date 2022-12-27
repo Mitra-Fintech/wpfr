ff294b4b91ca955991d24e498ebb6eb0a105ac84
Vivaan Shiromani <shiromanivivan@gmail.com>
2022-12-27 13:56:44 +0530

    employer/my-profile page job list api integrated

--- src/app/components/pages/my-profile-employer/company-details-page.component.ts
+++ src/app/components/pages/my-profile-employer/company-details-page.component.ts
@@ -10,6 +10,7 @@ import { Router } from '@angular/router';
 export class MyCompanyDetailsPageComponent implements OnInit {
   public recentJobs: any;
   public recentJobsSize: any;
+  public recentJobsLimitArray: any[] = [];
 
   constructor(private http: HttpClient, private router: Router) { }
 
@@ -23,45 +24,56 @@ export class MyCompanyDetailsPageComponent implements OnInit {
 
     let user_id = sessionStorage.getItem('userId') || 'no-user-id';
 
-    user_id = user_id.replace('"', '').replace('"', '');
+        user_id = user_id.replace('"', '').replace('"', '');
 
-    this.http
-                .get('https://workfromhome.world/api/job/list' + '?company_id=' + user_id')
-                .subscribe((response) => {
+        this.http
+            .get('https://workfromhome.world/api/job/list' + '?company_id=' + user_id)
+            .subscribe((response) => {
+                interface ResponseObject {
+                    status: string;
+                    code: any;
+                    data: Object;
+                    // session_id: string;
+                }
 
-                    interface ResponseObject {
-                        status: string;
-                        code: any;
-                        data : Object;
-                        // session_id: string;
+                interface DataArrayObject {
+                    // job_title: string;
+                    array: Object;
+                }
+
+                let responseObj: ResponseObject = JSON.parse(
+                    JSON.stringify(response)
+                );
+
+                let dataJson: DataArrayObject = JSON.parse(
+                    JSON.stringify(responseObj.data)
+                );
+
+                // localStorage.setItem('job_listing_data',JSON.stringify(dataJson));
+
+                this.recentJobs = Object.entries(dataJson);
+                
+                if(this.recentJobs.length == 0) {
+                    // this.empDasharraySize = 0;
+                }
+                
+                else if(this.recentJobs.length == 1) {
+                    this.recentJobsLimitArray.push(this.recentJobs[0]);
+                }
+                
+                else{
+                    for(let i = 0; i < 2; i++) {
+                        this.recentJobsLimitArray.push(this.recentJobs[i]);
                     }
+                }
 
-                    interface DataArrayObject {
-                        // job_title: string;
-                        array: Object;
-                        
+                // this.empDasharraySize = localStorage.getItem('active_jobs');
+                
+                // this.empDasharraySize = this.empDashArray.length;
+                // console.log(this.empDashArray[2][1]);
 
-                    }
-
-                    let responseObj: ResponseObject = JSON.parse(
-                        JSON.stringify(response)
-                    );
-
-                    let dataJson: DataArrayObject = JSON.parse(
-                        JSON.stringify(responseObj.data)
-                    );
-                    
-                    // localStorage.setItem('job_listing_data',JSON.stringify(dataJson));
-
-                    this.recentJobs = Object.entries(dataJson);
-
-                    this.recentJobsSize = this.recentJobs.length
-
-                    console.log(this.recentJobs[2][1]);  
-                    
-                    // console.log(responseObj.status);
-
-                });
+                // console.log(responseObj.status);
+            });
 
               }
 
